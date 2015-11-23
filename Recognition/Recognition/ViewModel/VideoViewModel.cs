c3947cabe5c6b94b41e02e2fe17a1b17cfcb072d
NatalliaBarysevich <nataliaborisevich@gmail.com>
2015-11-24 01:02:30 +0300

    Add : ApplyMask - for showing subtraction GetFlow - get video flow

--- Recognition/Recognition/ViewModel/VideoViewModel.cs
+++ Recognition/Recognition/ViewModel/VideoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 using System;
+using System.Linq;
 using NReco.VideoConverter;
 using System.IO;
 using GalaSoft.MvvmLight;
@@ -11,17 +12,14 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Collections;
 using Recognition.Model;
+using System.Diagnostics;
 
 namespace Recognition.ViewModel
 {
-    public class VideoViewModel : ViewModelBase, IDisposable
+    public class VideoViewModel : ViewModelBase
     {
-        // White pixel - object, black - background
-        const int ObjectBit = 1;
-        const int BackgroundBit = 0;
-        const int Scale = 5; 
-        private const double TimerInterval = 1;
-        private const double Range = 0.2;
+        private const double TimerInterval = 0.2;
+        private const int Scale = 3;
 
         private string _filePath;
         public string FilePath 
@@ -32,22 +30,22 @@ namespace Recognition.ViewModel
             }
             set
             {
-                if (Background != null)
-                    Background.UnlockBits();
                 RealFrame = null;
                 SubstarectedFrame = null;
                 _filePath = value;
                 RaisePropertyChanged("FileName");
                 CurrentPosition = 0;
-                Background = GetBitmap(0);
-                Background.LockBits();
+                FastBitmap background = GetBitmap(0);
+                if (background != null)
+                    _vibeModel.Initialize(background);                    
+                CurrentPosition += (float)TimerInterval;
             }
         }
+
         public string FileName
         {
             get { return Path.GetFileName(FilePath); }
         }
-        public FastBitmap Background { get; set; }
 
         private Bitmap _realFrame;
         public Bitmap RealFrame
@@ -77,6 +75,11 @@ namespace Recognition.ViewModel
                 RaisePropertyChanged("SubstarectedFrame");
             }
         }
+
+        private FastBitmap _nextFrame = null;
+        private byte[] _nextSubMask = null;
+        private FastBitmap _nextSubFrame = null;
+
         private float _currentPosition = -1;
         public float CurrentPosition
         {
@@ -89,147 +92,39 @@ namespace Recognition.ViewModel
             }
         }
 
-        private Harries harries;
-        private LucasKanadeMethod lucasKanade;
+        private Harries _harries;
+        private LucasKanadeMethod _lucasKanade;
+        private VibeModel _vibeModel;
 
         public VideoViewModel()
         {
             CurrentPosition = -1;
             LoadCommand = new RelayCommand(Load);
             GetNextFrameCommand = new RelayCommand(GetNextFrame);
-            harries = new Harries();
-            lucasKanade = new LucasKanadeMethod();
+            _vibeModel = new VibeModel();
+            _harries = new Harries();
+            _lucasKanade = new LucasKanadeMethod();
         }
 
-        private double GetDeviation(FastBitmap bitmap)
-        {
-            double sum = 0;
-            int count = bitmap.Height * bitmap.Width;
-
-            for (int x = 0; x < bitmap.Width; x++)
-                for (int y = 0; y < bitmap.Height; y++)
-                {
-                    System.Drawing.Color c = Background.GetPixel(x,y);
-                    sum += c.GetBrightness();
-                }
-
-            double expectation = sum/count;
-            sum = 0;
-            for (int x = 0; x < bitmap.Width; x++)
-                for (int y = 0; y < bitmap.Height; y++)
-                {
-                    System.Drawing.Color c = Background.GetPixel(x, y);
-                    sum += (c.GetBrightness() - expectation)*(c.GetBrightness() - expectation);
-                }
-            double variance = sum/count;
-            return Math.Sqrt(variance);
-        }
-
-        private int[,] Subtraction(FastBitmap current, double range)
-        {
-            int[,] result = new int[current.Width, current.Height];
-            for (int x =0; x< current.Width; x++)
-            {
-                for(int y = 0; y < current.Height; y++)
-                {
-                    System.Drawing.Color backgroundPixel = Background.GetPixel(x, y);
-                    System.Drawing.Color currentPixel = current.GetPixel(x, y);
-                    double delta = Math.Abs(currentPixel.GetBrightness() - backgroundPixel.GetBrightness());
-                    if (delta > range)
-                        result[x,y] = ObjectBit;
-                    else
-                        result[x,y] = BackgroundBit;
-                }
-            }
-            return result;
-        }
-        private Bitmap ConvertIntsToBitmap(int[,] map,FastBitmap frame)
-        {
-            Bitmap result = new Bitmap(map.GetLength(0), map.GetLength(1));
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    if (map[i, j] == ObjectBit)
-                        result.SetPixel(i, j, frame.Bitmap.GetPixel(i, j));
-                    else
-                        result.SetPixel(i, j, Color.Black);
-                }
-            }
-            return result;
-        }
-
-        private int[,] Closing(int[,] map)
-        {            
-            int[,] result = new int[map.GetLength(0), map.GetLength(1)];
-            for (int i = 0; i < map.GetLength(0); i++ )
-            {
-                for (int j = 0 ; j < map.GetLength(1); j++)
-                {
-                    result[i, j] = SetPixel(map, i, j);
-                }
-            }
-            return result;
-        }
-
-        private int SetPixel(int[,] map, int x, int y)
-        {
-            int[,] matrix = new int[,]
-            {{0,1,0},
-             {1,1,1},
-             {0,1,0}
-            };
-            int result = 1;
-            int xLen = matrix.GetLength(0);
-            int yLen = matrix.GetLength(1);
-            int[,] area = new int[xLen, yLen];
-            int ai = 0, bi = 0;
-            for (int i = x - xLen / 2; i <= x + xLen / 2; i++)
-            {
-                for (int j = y - yLen/2; j <= y + yLen/2; j++)
-                {
-                    if (i < 0 || i >= map.GetLength(0) || j < 0 || j >= map.GetLength(1))
-                    {
-                        area[ai, bi] = 0;
-                    }
-                    else
-                        area[ai, bi] = map[i, j];
-                    bi++;
-                }
-                bi = 0;
-                ai++;
-            }
-
-            for (int i = 0 ; i < xLen && result == 1; i++)
-            {
-                for (int j = 0 ; j < yLen && result == 1; j++)
-                {
-                    if (matrix[i, j] == 1)
-                        result &= matrix[i, j] & area[i, j];
-                }
-            }
-
-            return result;
-        }
         private FastBitmap GetBitmap(float time)
         {
             FastBitmap result;
             using (var stream = new MemoryStream())
             {
-                result = new FastBitmap(GetBitmap(time, stream));
+                result = GetBitmap(time, stream);
             }
             return result;
         }
 
-        private Bitmap GetBitmap(float time, MemoryStream stream)
+        private FastBitmap GetBitmap(float time, MemoryStream stream)
         {
-            Bitmap result = null;
+            FastBitmap result = null;
             var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
             ffMpeg.GetVideoThumbnail(FilePath, stream, time);
             if (stream.Length != 0)
             {
                 Image img = Image.FromStream(stream);
-                result = new Bitmap(new Bitmap(img, img.Width / Scale, img.Height / Scale));
+                result = new FastBitmap(new Bitmap(img, img.Width/Scale , img.Height/Scale));
             }
             return result;
         }
@@ -240,7 +135,6 @@ namespace Recognition.ViewModel
         private void Load()
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-
             dlg.InitialDirectory = @"Resources";
             dlg.DefaultExt = ".mp4";
             dlg.Filter = "Video files |*.avi; *.mp4; ";
@@ -253,48 +147,77 @@ namespace Recognition.ViewModel
 
         private void GetNextFrame()
         {
+            bool isNext= true;
+            FastBitmap realFrame = null;
+            byte[] subtractedMask = null;
+            FastBitmap subFrame = null;
             using (MemoryStream stream = new MemoryStream())
-            { 
-                RealFrame = GetBitmap(CurrentPosition, stream);
-                if (RealFrame != null)
+            {
+                if (CurrentPosition - TimerInterval < 0.0001)
                 {
-                    FastBitmap realFrame = new FastBitmap(RealFrame);
-                    Bitmap frame = SubstracteBackground(realFrame);
-                    List<Point> corners = harries.Corner(frame);
-                    if (corners.Count > 0)
-                    {
-                        FastBitmap nextFrame = GetBitmap(CurrentPosition + (float)TimerInterval);
-                        if (nextFrame != null)
-                        {
-                            Bitmap nextSubstarectedFrame = SubstracteBackground(nextFrame);
-                            lucasKanade.GetImageWithDisplacement(frame, nextSubstarectedFrame, corners);
-                        }
-                    }
-                    SubstarectedFrame = frame;
-                    CurrentPosition += (float)TimerInterval;                    
+                    realFrame = GetBitmap(CurrentPosition, stream);
+                    subtractedMask = _vibeModel.GetMask(realFrame);
+                    subFrame = ApplyMask(subtractedMask, realFrame);
                 }
                 else
                 {
+                    realFrame = _nextFrame;
+                    subtractedMask = _nextSubMask;
+                    subFrame = _nextSubFrame;
+                }
+                isNext = GetFlow(subtractedMask, realFrame,subFrame, stream);
+                if (!isNext)
+                {
                     CurrentPosition = -1;
                     SubstarectedFrame = null;
+                    _nextFrame = null;
                 }
             }
         }
 
-        private Bitmap SubstracteBackground(FastBitmap source)
+        private bool GetFlow(byte[] subMask, FastBitmap realFrame,FastBitmap subFrame, MemoryStream stream)
         {
-            source.LockBits();
-            var arr = Subtraction(source, Range);
-            source.UnlockBits();
-            arr = Closing(arr);
-            return ConvertIntsToBitmap(arr, source);
+            bool isNext = true;
+            List<Point> corners = _harries.Corner(subMask, subFrame.Width, subFrame.Height);
+            if (corners.Count > 0)
+            {
+                _nextFrame = GetBitmap(CurrentPosition + (float)TimerInterval, stream);
+                if (_nextFrame != null)
+                {
+                    _nextSubMask = _vibeModel.GetMask(_nextFrame);
+                    _nextSubFrame = ApplyMask(_nextSubMask, _nextFrame);
+                    var flow = _lucasKanade.GetImageWithDisplacement(subFrame, _nextSubFrame, corners);
+                    SubstarectedFrame = flow.Source;
+                    RealFrame = _nextFrame.Source;
+                    CurrentPosition += (float)TimerInterval;
+                }
+                else
+                    isNext = false;
+            }
+            return isNext;
         }
 
-        //TODO: save all images in directories, then try show it as video
-
-        public void Dispose()
+        private FastBitmap ApplyMask(byte[] mask, FastBitmap img)
         {
-            Background.UnlockBits();
+            Bitmap result = new Bitmap(img.Width, img.Height);
+            FastBitmap fb = new FastBitmap(result);
+            fb.LockBits();
+            img.LockBits();
+            int n = 0;
+            for (int j = 0; j < img.Height; j++)
+            {
+                for (int i = 0; i < img.Width; i++)
+                {
+                    if (mask[n] > VibeModel.BackgroundByte)
+                    {
+                        fb.SetPixel(i, j, img.GetPixel(i, j));
+                    }
+                    n++;
+                }
+            }
+            img.UnlockBits();
+            fb.UnlockBits();
+            return fb;
         }
     }
 }
