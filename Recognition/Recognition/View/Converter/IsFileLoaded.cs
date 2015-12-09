006739d40019ed8380a9258ce084bccffcf8c0d4
NatalliaBarysevich <nataliaborisevich@gmail.com>
2015-12-09 13:30:10 +0300

    replace IsNextFrame with IsLoadedFile

---
+++ Recognition/Recognition/View/Converter/IsFileLoaded.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Data;
+
+
+namespace Recognition.View.Converter
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    class IsFileLoaded : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !string.IsNullOrEmpty((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
